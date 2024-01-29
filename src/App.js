556598f46397fed83db2843e71cdf3057422f1f1
unknown <spandana.kf15@gmail.com>
2024-01-28 18:15:11 -0700

    added server.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
 
-function App() {
+
+    const getMessages = async () => {
+      const options = {
+        method: "POST",
+        body: JSON.stringify({
+          message: "Hello, how are you?"
+        }),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+      try{
+        const response =  await fetch('http://localhost:8000/completions', options)
+        const data = response.json()
+      }catch(error) {
+        console.error(error)
+      }
+    }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="side-bar">
+        <button>+ New Chat</button>
+        <ul className="history">
+          <li>First Chat</li>
+        </ul>
+        <nav>
+          <p>Made by KK</p>
+        </nav>
+      </section>
+      <section className="main">
+        <h1>KK ChatBot</h1>
+        <ul className="feed">
+          
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input/>
+            <div id="submit" onClick={getMessages}>↣</div>
+          </div>
+          <p className="info">
+            This Chatbot is made using GPT-3.5 turbo Model
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
